592c7ce35bd04f4619765e3b3c8c359a21194e96
raveena04 <58988696+raveena04@users.noreply.github.com>
2020-11-22 23:28:27 +0530

    Update Tokenization,Lemmetization and Stemming.py

--- Tokenization,Lemmetization and Stemming.py
+++ Tokenization,Lemmetization and Stemming.py
@@ -30,7 +30,7 @@ for words in tokens:
 #Lemmatization
 import spacy
 nlp = spacy.load('en_core_web_sm')
-
+/* */
 def show_lemmas(text):
     for token in text:
         print(f'{token.text:{12}} {token.pos_:{6}} {token.lemma:<{22}} {token.lemma_}')
